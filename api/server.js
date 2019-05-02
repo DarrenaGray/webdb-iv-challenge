e723cccf33c7e1d74d5c591711f4d16c239222bf
Darrena Gray <40064931+WhatTheSpork@users.noreply.github.com>
2019-05-02 18:20:03 -0400

    Imported recipes router and created base url for recipes

--- api/server.js
+++ api/server.js
@@ -5,12 +5,14 @@ const morgan = require('morgan');
 const server = express();
 
 const dishesRouter = require('../dishes/dishes-router');
+const recipesRouter = require('../recipes/recipes-router');
 
 server.use(express.json());
 server.use(helmet());
 server.use(morgan('dev'));
 
 server.use('/api/dishes', dishesRouter);
+server.use('/api/recipes', recipesRouter);
 
 server.get('/', (req, res) => {
     res.send('Server is running!');
