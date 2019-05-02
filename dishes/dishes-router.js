e609d49a993eb1783f40fb8fb2b64249839ec3f3
Darrena Gray <40064931+WhatTheSpork@users.noreply.github.com>
2019-05-02 19:05:55 -0400

    Created endpoint GET for returning recipes by Id with related dishes. Need to change the configurations and request so that it will return the dish by Id with the related recipe

--- dishes/dishes-router.js
+++ dishes/dishes-router.js
@@ -41,4 +41,20 @@ router.post('/', (req, res) => {
         });
 });
 
+router.get('/:id/recipes', (req, res) => {
+    const { id } = req.params;
+    Dishes
+        .getDishRecipes(id)
+        .then(dish => {
+            if (dish.length !== 0) {
+                res.status(200).json(dish);
+            } else {
+                res.status(404).json({ message: "The dish with the specified ID could not be found." })
+            }
+        })
+        .catch(err => {
+            res.status(500).json({ err, message: "There was a problem getting the recipes for this dish." });
+        });
+})
+
 module.exports = router;
