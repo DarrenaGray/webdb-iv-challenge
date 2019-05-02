4fa8cc57b9abebe02cc332a91d107bdad6ed8df1
Darrena Gray <40064931+WhatTheSpork@users.noreply.github.com>
2019-05-02 18:45:52 -0400

    Created endpoint POST for adding a new recipe with its respective dish id to the database

--- recipes/recipes-router.js
+++ recipes/recipes-router.js
@@ -6,8 +6,20 @@ router.get('/', (req, res) => {
     Recipes
         .getRecipes()
         .then(recipes => {
-            res.status(200).json(recipes)
+            res.status(200).json(recipes);
         });
 });
+
+router.post('/', (req, res) => {
+    const { dish_id, name } = req.body;
+    Recipes
+        .addRecipe({ dish_id, name })
+        .then(recipe => {
+            res.status(201).json({ recipe, message: "The recipe was successfully added!" });
+        })
+        .catch(err => {
+            res.status(500).json({ err, message: "There was a problem adding the recipe." });
+        });
+})
 
 module.exports = router;
