c8ea51a631fce1f6d8ee8611b0df77ab473f9af6
Darrena Gray <40064931+WhatTheSpork@users.noreply.github.com>
2019-05-02 17:53:38 -0400

    Created endpoint GET for requesting a dish by Id

--- dishes/dishes-router.js
+++ dishes/dishes-router.js
@@ -13,4 +13,20 @@ router.get('/', (req, res) => {
         });
 });
 
+router.get('/:id', (req, res) => {
+    const { id } = req.params
+    Dishes
+        .findById(id)
+        .then(dish => {
+            if (dish) {
+                res.status(200).json(dish)
+            } else {
+                res.status(404).json({ message: "The dish with the specified ID could not be found." })
+            }
+        })
+        .catch(err => {
+            res.status(500).json({ err, message: "There was a problem retrieving the dish." })
+        });
+});
+
 module.exports = router;
